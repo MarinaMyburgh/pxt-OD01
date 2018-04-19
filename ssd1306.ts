dc8f5d1c04824c5666ce56bbec8df9f6fb937ac0
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2018-04-19 22:17:50 +0200

    Update ssd1306.ts

--- ssd1306.ts
+++ ssd1306.ts
@@ -4,14 +4,12 @@ namespace OD01 {
 
     /**
      * initialises the i2c OD01 display
-     * @param height height (in pixels)
-     * @param width width (in pixels)
      */
     //% blockId=oled_init_terminal
-    //% block="initialize OD01 with height %height|width %width"
+    //% block="initialize OD01
     //% icon="\u2612" 
     //% shim=OD01::init_terminal
-    export function init(height: number, width: number): void {
+    export function init(): void {
         return;
     }
 
