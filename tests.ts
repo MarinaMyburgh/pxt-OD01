53a16e090a59f010bfd1c1c479372356da1adf62
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2018-04-19 22:19:21 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,6 +1,6 @@
 // tests go here; this will not be compiled when this package is used as a library
 
-OD01.init(64, 128)
+OD01.init()
 OD01.showString("hello, world!")
 OD01.showString("counter:")
 let item = 0
